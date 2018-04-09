c7cf849d90b7880c6c384810ba1157ff79859f38
Vimsucks <dev@vimsucks.com>
2018-04-09 18:16:37 +0800

    show different intension if query param 'company' is set

---
+++ source/static/js/resume.js
@@ -0,0 +1,11 @@
+var jobIntensions = {
+  "default": "软件开发工程师实习生",
+  "huawei": "IT应用软件开发工程师实习生",
+}
+var href = window.location.href;
+var url = new URL(href);
+var company = url.searchParams.get("company")
+if (!(company in jobIntensions)) {
+  company = "default"
+}
+$("h2").text(jobIntensions[company])
