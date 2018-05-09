8385e098be70bff09cc767f1db3386f57d467e42
Vimsucks <dev@vimsucks.com>
2018-05-09 10:16:35 +0800

    update resume

--- source/static/js/resume.js
+++ source/static/js/resume.js
@@ -1,12 +1,14 @@
 var jobIntensions = {
-  "default": "软件开发工程师实习生",
-  "huawei": "IT应用软件开发工程师实习生",
-  "cainiao": "研发工程师JAVA实习生",
+    "default": "软件开发工程师实习生",
+    "huawei": "IT应用软件开发工程师实习生",
+    "cainiao": "研发工程师JAVA实习生",
+    "meili": "后端开发实习生",
+    "dianwoda": "Java开发实习生",
 }
 var href = window.location.href;
 var url = new URL(href);
 var company = url.searchParams.get("company")
 if (!(company in jobIntensions)) {
-  company = "default"
+    company = "default"
 }
 $("h2").text(jobIntensions[company])
