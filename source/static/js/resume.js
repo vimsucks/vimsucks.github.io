5e6ae993eba677cdc0e7108e1e65bb99b5670651
Vimsucks <dev@vimsucks.com>
2018-08-29 21:29:15 +0800

    update resume.js

--- source/static/js/resume.js
+++ source/static/js/resume.js
@@ -1,9 +1,13 @@
 var jobIntensions = {
-    "default": "Java开发工程师实习生",
+    "default": "Java开发工程师",
     "huawei": "IT应用软件开发工程师实习生",
     "cainiao": "研发工程师JAVA实习生",
     "meili": "后端开发实习生",
     "dianwoda": "Java开发实习生",
+    "netease": "Java 开发工程师",
+    "ali": "Java 后端开发",
+    "microstrategy": "Associate Software Engineer 开发工程师",
+    "dianwoda": "Java 开发工程师"
 }
 var href = window.location.href;
 var url = new URL(href);
